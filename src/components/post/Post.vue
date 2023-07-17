8628e409cf90491c2daef317e44a839d8a107e3c
flaim61 <flaim611@gmail.com>
2023-07-17 22:49:12 +0600

    Занятие 17.07

---
+++ src/components/post/Post.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="post">
+        <div class="d-flex justify-content-between">
+            <h3>
+                {{ this.title }} 
+            </h3>
+            <button type="button" class="close" @click='this.deletePost'>
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <p>
+            {{ this.text }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Post",
+        props: [
+            'title', 
+            'text',
+            'deletePost'
+        ],
+        components: {
+
+        },
+        data(){
+            
+        }
+    }
+</script>
+
+<style>
+    .post{
+        border: 1px solid black;
+        padding: 15px;
+        margin-top: 10px;
+    }
+    .post:last-child{
+        margin-bottom: 10px;
+    }
+</style>
